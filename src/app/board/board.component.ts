20dd0be8950cc8aaf622ce23dcab2f5b5b298b9e
Szymon Rafałowski <szymon2489@gmail.com>
2023-03-26 13:05:18 +0200

    all AIs work but hard and impossible super slow

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,6 +1,8 @@
+import { getLocaleFirstDayOfWeek } from '@angular/common'
 import { Component, OnInit, Input } from '@angular/core'
 
 // TODO: 3x3 rule in AI to improve performance
+// TODO: First AI also should move
 
 @Component({
   selector: 'app-board',
@@ -12,11 +14,13 @@ export class BoardComponent {
   @Input() public player1: string = ''
   @Input() public player2: string = ''
   private FIELD_SIZE: number = 50
+  private DELAY_MS: number = 100
   public board: number[][] = []
   public turn: number = 1
   public gameOver: boolean = false
   public canvas = document.getElementById('boardCanvas') as HTMLCanvasElement
   public textInfo = 'Player 1 turn'
+  public lastMove: number[] = []
 
   ngOnInit() {
     this.createBoard()
@@ -27,6 +31,9 @@ export class BoardComponent {
     if (this.boardSize === 19) {
       this.FIELD_SIZE = 40
     }
+    setTimeout(() => {
+      this.nextTurn()
+    }, this.DELAY_MS)
   }
 
   public createBoard(): void {
@@ -54,6 +61,12 @@ export class BoardComponent {
 
     for (let i = 0; i < this.boardSize; i++) {
       for (let j = 0; j < this.boardSize; j++) {
+        if (i === this.lastMove[0] && j === this.lastMove[1]) {
+          ctx.beginPath()
+          ctx.fillStyle = 'rgba(255, 0, 0, 0.5)'
+          ctx.rect(this.FIELD_SIZE * i, this.FIELD_SIZE * j, this.FIELD_SIZE, this.FIELD_SIZE)
+          ctx.fill()
+        }
         ctx.beginPath()
         ctx.moveTo(this.FIELD_SIZE * (i + 0.5), this.FIELD_SIZE * 0.5)
         ctx.lineTo(this.FIELD_SIZE * (i + 0.5), this.canvas.height - this.FIELD_SIZE * 0.5)
@@ -96,7 +109,8 @@ export class BoardComponent {
       const xGrid = Math.floor(x / this.FIELD_SIZE)
       const yGrid = Math.floor(y / this.FIELD_SIZE)
       if (this.board[xGrid][yGrid] !== 1 && this.board[xGrid][yGrid] !== 2) {
-        this.board[xGrid][yGrid] = this.turn === 1 ? 1 : 2
+        this.board[xGrid][yGrid] = this.turn
+        this.lastMove = [xGrid, yGrid]
         this.turn = this.turn === 1 ? 2 : 1
         this.updateCanvas()
         this.textInfo = this.turn === 1 ? 'Player 1 turn' : 'Player 2 turn'
@@ -105,7 +119,7 @@ export class BoardComponent {
 
         setTimeout(() => {
           this.nextTurn()
-        }, 10)
+        }, this.DELAY_MS)
       }
     }
   }
@@ -153,7 +167,7 @@ export class BoardComponent {
             this.textInfo = `Player ${color} wins!`
             setTimeout(() => {
               alert(`Player ${color} wins!`)
-            }, 10)
+            }, this.DELAY_MS)
             return
           }
         }
@@ -175,32 +189,46 @@ export class BoardComponent {
       this.textInfo = 'Draw!'
       setTimeout(() => {
         alert('Draw!')
-      }, 10)
+      }, this.DELAY_MS)
     }
   }
 
   public nextTurn(): void {
     if (!this.gameOver) {
       const player = this.turn === 1 ? this.player1 : this.player2
+      console.log(this.turn)
+
       if (player !== 'Human') {
         if (player === 'aiEasy') {
-          this.aiEasy()
-        } /*else if (player === 'aiMedium') {
-          this.aiMedium()
+          this.ai(2)
+        } else if (player === 'aiMedium') {
+          this.ai(3)
         } else if (player === 'aiHard') {
-          this.aiHard()
+          this.ai(4)
         } else if (player === 'aiImpossible') {
-          this.aiImpossible()
-        }*/
+          this.ai(5)
+        }
       }
     }
   }
 
   // use AI class to get the move
-  public aiEasy(): void {
-    const ai = new AI('easy', this.board, this.boardSize, this.turn)
+  public ai(depth: number): void {
+    const ai = new AI('easy', this.board, this.boardSize, this.turn, depth)
     const newBoard = ai.makeAIMove()
+    const oldBoard = this.board
     this.board = newBoard
+
+    // compare the new board with the old one to find the move
+    for (let i = 0; i < this.boardSize; i++) {
+      for (let j = 0; j < this.boardSize; j++) {
+        if (this.board[i][j] !== oldBoard[i][j]) {
+          this.lastMove = [i, j]
+          break
+        }
+      }
+    }
+
     this.turn = this.turn === 1 ? 2 : 1
     this.updateCanvas()
 
@@ -208,11 +236,13 @@ export class BoardComponent {
     this.checkWin()
     this.checkDraw()
 
-    this.nextTurn()
+    setTimeout(() => {
+      this.nextTurn()
+    }, this.DELAY_MS)
   }
 
   public mouseMove = (e: MouseEvent): void => {
-    if (this.gameOver) return
+    if (this.gameOver || !this.isHumanTurn) return
     const rect = this.canvas.getBoundingClientRect()
     const x = e.clientX - rect.left
     const y = e.clientY - rect.top
@@ -247,21 +277,20 @@ export class BoardComponent {
   }
 }
 
-// easy, medium, hard or impossible
-// minmax algorithm
 class AI {
   private difficulty: string
   private board: number[][]
   private boardSize: number
   private turn: number
   private maxDepth: number
+  private indexDepth = 0
 
-  constructor(difficulty: string, board: number[][], boardSize: number, turn: number) {
+  constructor(difficulty: string, board: number[][], boardSize: number, turn: number, maxDepth: number) {
     this.difficulty = difficulty
     this.board = board
     this.boardSize = boardSize
     this.turn = turn
-    this.maxDepth = 3
+    this.maxDepth = maxDepth
   }
 
   private checkWin(board: number[][]): number {
@@ -331,35 +360,31 @@ class AI {
     return emptyCells
   }
 
-  // 4 empty is [blank, player1, player1, player1, player1, blank]
-  // 3 empty is [blank, player1, player1, player1, blank]
+  private getNiceEmptyCells(board: number[][], color: number): number[][] {
+    let emptyCells = this.getEmptyCells(board)
+    let niceEmptyCells: number[][] = []
+    emptyCells.forEach((cell) => {
+      board[cell[0]][cell[1]] = color
+      if (this.checkWin(board) === color) {
+        console.log('nice')
 
-  // (player 2 can be also be a wall)
-  // 4 one side is [blank, player1, player1, player1, player1, player2] or [player2, player1, player1, player1, player1, blank]
-  // 3 one side is [blank, player1, player1, player1, player2] or [player2, player1, player1, player1, blank]
+        niceEmptyCells.unshift(cell)
+      } else {
+        niceEmptyCells.push(cell)
+      }
+      board[cell[0]][cell[1]] = 0
+    })
 
-  // 4 both sides is [player2, player1, player1, player1, player1, player2]
-  // 3 both sides is [player2, player1, player1, player1, player2]
+    return niceEmptyCells
+  }
 
-  // - points for opponent
-  // if 1 or more x 4 empty in a row, return 10
-  // if 2 or more x 3 empty in a row, return 10
-  // if 2 or more x 3 one side in a row, return 9
-  // if 1 or more x 4 one side in a row, return 8
-  // if 2 or more x 2 empty in a row, return 7
-  // if 2 or more x 2 one side in a row, return 6
-  // if 1 or more x 3 empty in a row, return 5
-  // if 1 or more x 3 one side in a row, return 4
-  // if 1 or more x 2 empty in a row, return 3
-  // if 1 or more x 2 one side in a row, return 2
-  // if 1 or more x 1 empty in a row, return 1
-  // if 1 or more x 1 one side in a row, return 0
+  private calculatePoints(board: number[][]): [number, number] {
+    let blocks = this.getBlocks(board)
 
-  // the same with another player but with -10, -9, -8, -7, -6, -5, -4, -3, -2, -1
+    return [this.getPointsFromBlocks(blocks[0]), this.getPointsFromBlocks(blocks[1])]
+  }
 
-  private calculatePoints(board: number[][]): number {
-    let points = 0
-
+  private getBlocks(board: number[][]): [any, any, any, any] {
     let yourBlocks = {
       b6plus: 0,
       b5: 0,
@@ -388,6 +413,9 @@ class AI {
       b2_2w: 0
     }
 
+    let yourNiceMoves: any[] = []
+    let opponentNiceMoves: any[] = []
+
     for (let i = 0; i < this.boardSize; i++) {
       for (let j = 0; j < this.boardSize; j++) {
         const color = board[i][j]
@@ -403,6 +431,8 @@ class AI {
         ]
 
         for (const [dx, dy] of directions) {
+          let potentiallyGoodMoves = []
+
           let count = 1
           let x = i + dx
           let y = j + dy
@@ -417,6 +447,8 @@ class AI {
 
           if (!(x >= 0 && x < this.boardSize && y >= 0 && y < this.boardSize && board[x][y] === 0)) {
             walls++
+          } else {
+            potentiallyGoodMoves.push([x, y])
           }
 
           x = i - dx
@@ -430,9 +462,11 @@ class AI {
 
           if (!(x >= 0 && x < this.boardSize && y >= 0 && y < this.boardSize && board[x][y] === 0)) {
             walls++
+          } else {
+            potentiallyGoodMoves.push([x, y])
           }
 
-          if (color === (this.turn === 1 ? 1 : 2)) {
+          if (color === 2) {
             if (count >= 5) {
               yourBlocks.b6plus++
             } else if (count === 5) {
@@ -440,22 +474,37 @@ class AI {
             } else if (count === 4) {
               if (walls === 0) {
                 yourBlocks.b4_0w++
+                potentiallyGoodMoves.forEach((move) => {
+                  yourNiceMoves.push(move)
+                })
               } else if (walls === 1) {
                 yourBlocks.b4_1w++
+                potentiallyGoodMoves.forEach((move) => {
+                  yourNiceMoves.push(move)
+                })
               } else if (walls === 2) {
                 yourBlocks.b4_2w++
               }
             } else if (count === 3) {
               if (walls === 0) {
                 yourBlocks.b3_0w++
+                potentiallyGoodMoves.forEach((move) => {
+                  yourNiceMoves.push(move)
+                })
               } else if (walls === 1) {
                 yourBlocks.b3_1w++
+                potentiallyGoodMoves.forEach((move) => {
+                  yourNiceMoves.push(move)
+                })
               } else if (walls === 2) {
                 yourBlocks.b3_2w++
               }
             } else if (count === 2) {
               if (walls === 0) {
                 yourBlocks.b2_0w++
+                potentiallyGoodMoves.forEach((move) => {
+                  yourNiceMoves.push(move)
+                })
               } else if (walls === 1) {
                 yourBlocks.b2_1w++
               } else if (walls === 2) {
@@ -470,22 +519,37 @@ class AI {
             } else if (count === 4) {
               if (walls === 0) {
                 opponentBlocks.b4_0w++
+                potentiallyGoodMoves.forEach((move) => {
+                  opponentNiceMoves.push(move)
+                })
               } else if (walls === 1) {
                 opponentBlocks.b4_1w++
+                potentiallyGoodMoves.forEach((move) => {
+                  opponentNiceMoves.push(move)
+                })
               } else if (walls === 2) {
                 opponentBlocks.b4_2w++
               }
             } else if (count === 3) {
               if (walls === 0) {
                 opponentBlocks.b3_0w++
+                potentiallyGoodMoves.forEach((move) => {
+                  opponentNiceMoves.push(move)
+                })
               } else if (walls === 1) {
                 opponentBlocks.b3_1w++
+                potentiallyGoodMoves.forEach((move) => {
+                  opponentNiceMoves.push(move)
+                })
               } else if (walls === 2) {
                 opponentBlocks.b3_2w++
               }
             } else if (count === 2) {
               if (walls === 0) {
                 opponentBlocks.b2_0w++
+                potentiallyGoodMoves.forEach((move) => {
+                  opponentNiceMoves.push(move)
+                })
               } else if (walls === 1) {
                 opponentBlocks.b2_1w++
               } else if (walls === 2) {
@@ -497,35 +561,27 @@ class AI {
       }
     }
 
-    if (this.getPointsFromBlocks(yourBlocks) > 9) {
-      points = 10
-    } else if (this.getPointsFromBlocks(opponentBlocks) > 9) {
-      points = -9
-    } else {
-      points = this.getPointsFromBlocks(yourBlocks) - this.getPointsFromBlocks(opponentBlocks)
-    }
-
-    return points
+    return [yourBlocks, opponentBlocks, yourNiceMoves, opponentNiceMoves]
   }
 
-  getPointsFromBlocks(blocks: any): number {
+  private getPointsFromBlocks(blocks: any): number {
     let points = 0
-    if (blocks.b6plus > 0) {
-      points = 10
-    } else if (blocks.b5 > 0) {
-      points = 10
+    if (blocks.b5 > 0) {
+      console.log('ccccccccccccc')
+
+      points = 100
     } else if (blocks.b4_0w > 0) {
-      points = 10
+      points = 90
     } else if (blocks.b4_1w > 1) {
-      points = 10
+      points = 80
     } else if (blocks.b3_0w > 2) {
-      points = 10
+      points = 70
     } else if (blocks.b3_0w > 1) {
-      points = 9
+      points = 60
     } else if (blocks.b3_0w > 0) {
-      points = 8
+      points = 50
     } else if (blocks.b4_1w > 0) {
-      points = 7
+      points = 40
     } else if (blocks.b4_2w > 4) {
       points = 6
     } else if (blocks.b2_0w > 3) {
@@ -561,8 +617,7 @@ class AI {
     return points
   }
 
-  private indexDepth = 0
-  private minmax(board: number[][], depth: number, alpha: number, beta: number, isMaximizing: boolean): number {
+  private minimax(board: number[][], depth: number, alpha: number, beta: number, isMaximizing: boolean): number {
     this.indexDepth++
     if (this.indexDepth % 1000000 === 0) {
       console.log(this.indexDepth)
@@ -570,28 +625,39 @@ class AI {
 
     let result = this.checkWin(board)
     if (result !== 0) {
-      return result === 1 ? 10 : -10
+      //console.log('win')
+      //console.log(JSON.parse(JSON.stringify(board)))
+      //console.log(result)
+
+      return result === 2 ? 1000 : -1000
     }
 
     if (this.checkDraw(board)) {
       return 0
     }
 
+    let points = this.calculatePoints(board)
+    if (points[0] > 90 && !isMaximizing) {
+      //console.log('aaaaa')
+
+      return points[0]
+    } else if (points[1] < -9 && isMaximizing) {
+      console.log('bbbbb')
+
+      //return points[1]
+    }
+
     if (depth === this.maxDepth) {
-      // return this.calculatePoints(board)
-      if (isMaximizing) {
-        return -this.calculatePoints(board)
-      } else {
-        return this.calculatePoints(board)
-      }
+      return points[0] - points[1]
     }
 
     if (isMaximizing) {
       let bestScore = -Infinity
-      const emptyCells = this.getEmptyCells(board)
+      //const emptyCells = this.getEmptyCells(board)
+      const emptyCells = this.getNiceEmptyCells(board, 2)
       for (const [i, j] of emptyCells) {
-        board[i][j] = 1
-        let score = this.minmax(board, depth + 1, alpha, beta, false)
+        board[i][j] = 2
+        let score = this.minimax(JSON.parse(JSON.stringify(board)), depth + 1, alpha, beta, false)
         board[i][j] = 0
         bestScore = Math.max(score, bestScore)
         alpha = Math.max(alpha, score)
@@ -599,6 +665,7 @@ class AI {
           break
         }
 
+        /*
         let isEmpty7x7 = true
         for (let y2 = -3; y2 < 4; y2++) {
           for (let x2 = -3; x2 < 4; x2++) {
@@ -614,15 +681,16 @@ class AI {
         }
         if (isEmpty7x7) {
           break
-        }
+        }*/
       }
       return bestScore
     } else {
       let bestScore = Infinity
-      const emptyCells = this.getEmptyCells(board)
+      //const emptyCells = this.getEmptyCells(board)
+      const emptyCells = this.getNiceEmptyCells(board, 1)
       for (const [i, j] of emptyCells) {
-        board[i][j] = 2
-        let score = this.minmax(board, depth + 1, alpha, beta, true)
+        board[i][j] = 1
+        let score = this.minimax(JSON.parse(JSON.stringify(board)), depth + 1, alpha, beta, true)
         board[i][j] = 0
         bestScore = Math.min(score, bestScore)
         beta = Math.min(beta, score)
@@ -630,6 +698,7 @@ class AI {
           break
         }
 
+        /*
         let isEmpty7x7 = true
         for (let y2 = -13; y2 < 14; y2++) {
           for (let x2 = -13; x2 < 14; x2++) {
@@ -645,7 +714,7 @@ class AI {
         }
         if (isEmpty7x7) {
           break
-        }
+        }*/
       }
       return bestScore
     }
@@ -654,19 +723,18 @@ class AI {
   private findBestMove(board: number[][], depth: number): number[] {
     let bestScore = -Infinity
     let move: number[] = []
-    for (let i = 0; i < this.boardSize; i++) {
-      for (let j = 0; j < this.boardSize; j++) {
-        if (board[i][j] === 0) {
-          board[i][j] = 1
-          let score = this.minmax(board, depth, -Infinity, Infinity, false)
-          board[i][j] = 0
-          if (score > bestScore) {
-            bestScore = score
-            move = [i, j]
-          }
-        }
+    //const emptyCells = this.getEmptyCells(board)
+    const emptyCells = this.getNiceEmptyCells(board, 2)
+    for (const [i, j] of emptyCells) {
+      board[i][j] = 2
+      let score = this.minimax(JSON.parse(JSON.stringify(board)), depth, -Infinity, Infinity, false)
+      board[i][j] = 0
+      if (score > bestScore) {
+        bestScore = score
+        move = [i, j]
       }
     }
+
     console.log('bestScore', bestScore)
 
     return move
